1d73bd34a37257666827c64d9f81a3fb999cb36a
AspectType <39821770+wortss@users.noreply.github.com>
2022-10-07 18:13:54 +0100

    Update helpers.lua

--- helpers.lua
+++ helpers.lua
@@ -1,1 +1,27 @@
-return {}
+local Helpers = {}
+
+function Helpers:FindPlayerByString(str)
+	local found = false
+	str = str:lower()
+	local length = str:len()
+
+	for _,v in pairs(game.Players:GetPlayers()) do
+		if v.Name:sub(1, length):lower() == str then
+			found = v
+			break    
+		end
+	end
+
+	for _,v in pairs(game.Players:GetPlayers()) do
+		if v.DisplayName:sub(1, length):lower() == str then
+			found = v
+			break    
+		end
+	end
+
+	return found
+	
+end
+
+
+return Helpers
